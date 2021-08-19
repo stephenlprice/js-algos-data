1456f19332ad8d89dda612e6b6a7d13c3475e80d
Stephen Price <58522233+stephenlprice@users.noreply.github.com>
2021-08-19 16:26:15 -0500

    added a stacks file

---
+++ stacks.js
@@ -0,0 +1,6 @@
+// js stack implementation from freecodecamp.org
+// https://www.youtube.com/watch?v=t2CEgPsws3U
+// https://codepen.io/beaucarnes/pen/yMBGbR?editors=0012
+
+/* Stacks: Last In First Out */
+
