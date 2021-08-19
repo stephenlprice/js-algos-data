60940174f9501d1dd5c97913d15b7e380feb9d14
Stephen Price <58522233+stephenlprice@users.noreply.github.com>
2021-08-19 18:05:23 -0500

    finished stacks exercise
    Signed-off-by: Stephen Price <58522233+stephenlprice@users.noreply.github.com>

--- stacks.js
+++ stacks.js
@@ -2,5 +2,45 @@
 // https://www.youtube.com/watch?v=t2CEgPsws3U
 // https://codepen.io/beaucarnes/pen/yMBGbR?editors=0012
 
-/* Stacks: Last In First Out */
+/* 
+Stacks: Last In First Out 
+
+Functions: push, pop, peek, length (or size)
+
+Exercise: find palindromes
+*/
+
+
+const isPalindrome = (word) => {
+  let letters = []; // this is the stack
+  let rword = ''; // reverse word
+
+  if (typeof word !== 'string') {
+    return console.error('Error: ' + word + ' is not a string!');
+  }
+
+  // add letters of word into stack
+  for (let i = 0; i < word.length; i++) {
+    letters.push(word[i]);
+  }
+  
+  // pop off the stack in reverse order
+  for (let i = 0; i < word.length; i++) {
+    rword += letters.pop();
+  }
+
+  if (rword === word) {
+    console.log(word + ' is a palindrome!');
+  } else {
+    console.log(word + ' is not a palindrome...');
+  }
+
+}
+
+isPalindrome('racecar');
+isPalindrome('hello');
+isPalindrome('world');
+isPalindrome('bob');
+isPalindrome(true);
+
 
